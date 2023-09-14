49b5754183acc9b73ddbba6d7e547b07ced9dc01
Karina <karina.steinberg@voco.ee>
2023-09-14 13:55:54 +0300

    Displayed an article by slug, fixed a bug, css problems

--- index.js
+++ index.js
@@ -1,40 +1,67 @@
 //application packages
-    const express = require('express')
-    const app = express()
+const express = require('express')
+const app = express()
 
-    const path = require('path')
-    //add template engine
-    const hbs = require('express-handlebars');
-    //setup template engine directory and files extensions
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view.engine','hbs');
-    app.engine('hbs',hbs.engine({
-        extname:'hbs',
-        defaultLayout:'main',
-        layoutsDir:__dirname+ '/views/layouts/',
-    }))
-    //setup static public directory
-    app.use(express.static('public'));
+const path = require('path')
+//add template engine
+const hbs = require('express-handlebars');
+//setup template engine directory and files extensions
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+}))
+//setup static public directory
+app.use(express.static('public'));
 
-    const mysql = require('mysql')
+const mysql = require('mysql')
 
-    const bodyParser = require('body-parser')
-    app.use(bodyParser.urlencoded({extended: true}))
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
 
 //create database connection
-let con = mysql.createConnection({
+const con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "qwerty",
-    database: "joga_mysql",
-})
+    database: "joga_mysql"
+});
 
-con.connect(function(err) {
+con.connect(function (err) {
     if (err) throw err;
     console.log("Connected to joga_mysql db")
 })
 
+//show all articles - index page
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result
+        console.log(articles)
+        res.render('index', {
+            articles: articles
+        })
+    })
+});
+
+//show article by this slug
+app.get('/article/:slug', (req, res) => {
+    let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`
+    let article
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        article = result
+        console.log(article)
+        res.render('article', {
+            article: article
+        })
+    });
+});
 //app start point
-app.listen(3307, (localhost) => {
-    console.log('App is started at http://localhost:3307')
+app.listen(3000, (localhost) => {
+    console.log('App is started at http://localhost:3000')
 });
